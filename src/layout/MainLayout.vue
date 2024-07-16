2dec35672daa8035142461732178b8b4a6a97e62
Ali Heider Swccho <ali.heider.swccho@gmail.com>
2024-07-16 14:28:16 +0600

    working on sidenav

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    Main Layout
-    <div>
+  <div class="main-wrapper">
+    <!--  Side nav component  -->
+    <SideNav />
+
+    <!--  Main Content  -->
+    <div class="main-content">
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
 
+import SideNav from "@/layout/includes/SideNav.vue";
+
 export default {
+  components: {SideNav}
 }
 </script>
---
+++ src/layout/includes/SideNav.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="sidenav">
+    <!--  Avatar  -->
+    <div class="avatar-wrapper">
+      <img :src="'./sidenav/user.jpg'" alt="">
+    </div>
+  </div>
+</template>
